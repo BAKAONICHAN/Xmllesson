02e01fb2b6fec2407bbde5e6434a373bc6de7c0c
Nurik <tanatov1989@mail.ru>
2017-12-22 16:12:03 +0600

    Добавьте файлы проекта.

---
+++ XML-LESSON/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XML_LESSON
+{
+    public class Car
+    {
+        public  Guid Id {get;set;} 
+        public string Mark { get; set; }
+        public DateTime MadeDate { get; set; }
+        public int Rating { get; set; }
+
+
+    }
+}
---
+++ XML-LESSON/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XML_LESSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument document = new XmlDocument();
+            document.Load(@"cars.xml");
+
+            var rootElement = document.LastChild;
+
+            XmlElement newCar = document.CreateElement("car");
+            rootElement.AppendChild(newCar);
+            document.Save("cars.xml");
+
+            //foreach (XmlElement element in rootElement)
+            //{
+            //    Car car = new Car();
+            //    car.Id = Guid.Parse(element.ChildNodes[0].InnerText);
+            //    car.Mark = element.ChildNodes[1].InnerText;
+            //    car.MadeDate = DateTime.Parse(element.ChildNodes[2].InnerText);
+            //    car.Rating = int.Parse(element.ChildNodes[3].Attributes["value"].InnerText);
+
+            //    //Car car2 = new Car()
+            //    //{
+            //    //    Id = Guid.Parse(element.ChildNodes[0].InnerText),
+            //    //    Mark = element.ChildNodes[1].InnerText,
+            //    //    MadeDate = DateTime.Parse(element.ChildNodes[2].InnerText),
+            //    //    Rating = int.Parse(element.ChildNodes[3].InnerText),
+            //    //};
+            //}
+
+        }
+    }
+}
